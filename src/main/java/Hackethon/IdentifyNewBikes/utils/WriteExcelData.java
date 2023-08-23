9c15c57f35748ab8d6d6450e3a14ecb160843eb1
Soham Das <66131928+Soham2020@users.noreply.github.com>
2023-08-23 22:09:01 +0530

    Update WriteExcelData.java - Excel done

--- src/main/java/Hackethon/IdentifyNewBikes/utils/WriteExcelData.java
+++ src/main/java/Hackethon/IdentifyNewBikes/utils/WriteExcelData.java
@@ -2,45 +2,94 @@ package Hackethon.IdentifyNewBikes.utils;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 
-import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class WriteExcelData {
-	
-	public static void writeExcelData(String s, int rowNum, int colNum, String sheetName) throws Exception {
-		String path = "C:\\Users\\2282058\\Downloads\\eclipse-java-2023-03-R-win32-x86_64 (1)\\eclipse\\IdentifyNewBikes\\test-output\\OutputData.xlsx";
+	static File f;
+	static FileInputStream fi;
+	static XSSFWorkbook workbook;
+	static XSSFSheet sheet;
+	static XSSFRow row;
+	static String path = "E:\\eclipse-jee-2023-06-R-win32-x86_64\\eclipse\\IdentifyNewBikes\\test-output\\OutputBikes.xlsx";
+	static FileOutputStream fos;
 
-		File f = new File(path);
-		FileInputStream fi = new FileInputStream(f);
-		
-		XSSFWorkbook workbook = new XSSFWorkbook(fi);
-		
-		XSSFSheet sheet;
-		// check weather sheet is existing or not
-		if(workbook.getSheetIndex(sheetName) == -1) {
-			sheet = workbook.createSheet(sheetName);
-		}else {
-			sheet = workbook.getSheet(sheetName);
+	public static void writeExcelTopCellBikes() throws Exception {
+
+		f = new File(path);
+		fi = new FileInputStream(f);
+
+		workbook = new XSSFWorkbook(fi);
+
+		sheet = workbook.getSheet("Bikes");
+		row = sheet.createRow(0);
+		row.createCell(0).setCellValue("Bike's Name");
+		row.createCell(1).setCellValue("Price");
+		row.createCell(2).setCellValue("Launched Date");
+		fos = new FileOutputStream(f);
+		workbook.write(fos);
+	}
+
+	public static void writeExcelTopCellCars() throws Exception {
+
+		f = new File(path);
+		fi = new FileInputStream(f);
+
+		workbook = new XSSFWorkbook(fi);
+
+		sheet = workbook.createSheet("Cars");
+		row = sheet.createRow(0);
+		row.createCell(0).setCellValue("Car's Name");
+		fos = new FileOutputStream(f);
+		workbook.write(fos);
+	}
+
+	public static void writeExcelData(String[] s, int rowNum, String sheetName) throws Exception {
+
+		f = new File(path);
+		fi = new FileInputStream(f);
+
+		workbook = new XSSFWorkbook(fi);
+
+		sheet = workbook.getSheet(sheetName);
+
+		row = sheet.createRow(rowNum);
+
+		row.createCell(0).setCellValue(s[0]);
+		row.createCell(1).setCellValue(s[1]);
+		row.createCell(2).setCellValue(s[2]);
+
+		for (int i = 0; i < s.length; i++) {
+			row.createCell(i).setCellValue(s[i]);
 		}
-		
-		XSSFRow row;
-		// check weather row exists or not
-		if(sheet.getRow(rowNum) == null) {
-			row = sheet.createRow(rowNum);
+
+		fos = new FileOutputStream(f);
+		workbook.write(fos);
+
+	}
+
+	public static void writeExcelData(String s, String sheetName) throws Exception {
+
+		f = new File(path);
+		fi = new FileInputStream(f);
+
+		workbook = new XSSFWorkbook(fi);
+
+		sheet = workbook.getSheet(sheetName);
+
+		int rowNum = 1;
+		String[] arr = s.split("\n");
+		for (int i = 0; i < arr.length; i++) {
+			row = sheet.createRow(rowNum++);
+			row.createCell(0).setCellValue(arr[i]);
 		}
-		else {
-			row = sheet.getRow(rowNum);
-		}
-		
-		
-		
-		row.createCell(0).setCellValue("Bike Name");
-		
+
+		fos = new FileOutputStream(f);
+		workbook.write(fos);
+
 	}
 
 }
